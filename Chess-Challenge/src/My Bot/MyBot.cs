c2247f95dc04d301964f4b483a52cc5f7763a104
joshua_beard <matinzzz@icloud.com>
2023-07-27 23:53:00 -0700

    .

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -6,26 +6,29 @@ public class MyBot : IChessBot
 {
 
     // prep for 50 move game
-    const int timePerMove = 1200; //MS
+    const int timePerMove = 600; //MS 1200
     static IDictionary <ulong, double> transposition = new Dictionary<ulong, double>(); // should be an array ;-;
+
+    static int[] scores = {1, 3, 3, 5, 9};
 
     public Move Think(Board board, Timer timer)
     {
         Move best = Move.NullMove;
         for (int i = 1; timer.MillisecondsElapsedThisTurn < timePerMove; i++){
-            best = Search(board, timer, board.GameMoveHistory[board.GameMoveHistory.GetLength(0) - 1], i, int.MinValue, int.MaxValue, board.IsWhiteToMove).best;
+            best = Search(board, timer, Move.NullMove, i, int.MinValue, int.MaxValue, board.IsWhiteToMove).best;
+            Console.WriteLine(i);
         }
         return best;
     }
 
 
     
-    public SearchResult Search(Board board, Timer timer, Move lastMove, int depth, int a, int b, bool maximizer){
+    public SearchResult Search(Board board, Timer timer, Move lastMove, int depth, double a, double b, bool maximizer){
         if (depth == 0 || board.IsInCheckmate() || board.IsDraw())
         {
             return new SearchResult(lastMove, StaticEvaluation(board));
         }
-
+        
         if (maximizer)
         {
             SearchResult max = new SearchResult(Move.NullMove, int.MinValue);
@@ -34,6 +37,7 @@ public class MyBot : IChessBot
             {
                 board.MakeMove(move);
                 SearchResult result = Search(board, timer, move, depth - 1, a, b, false);
+                result.best = move;
                 board.UndoMove(move);
 
                 max = result.Max(max);
@@ -54,6 +58,7 @@ public class MyBot : IChessBot
             {
                 board.MakeMove(move);
                 SearchResult result = Search(board, timer, move, depth - 1, a, b, true);
+                result.best = move;
                 board.UndoMove(move);
 
                 min = result.Min(min);
@@ -68,16 +73,61 @@ public class MyBot : IChessBot
         }
     }
 
-    public int StaticEvaluation(Board b){
-        return 0;
+    private double StaticEvaluation(Board board)
+    {
+        int eval = 0;
+
+        if (board.IsDraw())
+        {
+            return 0;
+        }
+
+
+        if (board.IsWhiteToMove)
+        {
+            if(board.IsInCheckmate()){
+                return int.MinValue;
+            }
+            if(board.IsInCheck()){
+                eval -= 1;
+            }
+        }
+
+        if (!board.IsWhiteToMove)
+        {
+            if(board.IsInCheckmate()){
+                return int.MaxValue;
+            }
+            if(board.IsInCheck()){
+                eval += 1;
+            }
+        }
+
+
+
+        eval += scores[0] * board.GetPieceList(PieceType.Pawn, true).Count +
+                (scores[1] + (1/8) * board.GetPieceList(PieceType.Pawn, true).Count - 1) * board.GetPieceList(PieceType.Knight, true).Count +
+                scores[2] * board.GetPieceList(PieceType.Bishop, true).Count +
+                (scores[3] + (-1/8) * board.GetPieceList(PieceType.Pawn, true).Count + 1) * board.GetPieceList(PieceType.Rook, true).Count +
+                scores[4] * board.GetPieceList(PieceType.Queen, true).Count;
+
+
+        eval -= scores[0] * board.GetPieceList(PieceType.Pawn, false).Count +
+                (scores[1] + (1/8) * board.GetPieceList(PieceType.Pawn, true).Count - 1) * board.GetPieceList(PieceType.Knight, false).Count +
+                scores[2] * board.GetPieceList(PieceType.Bishop, false).Count +
+                (scores[3] + (-1/8) * board.GetPieceList(PieceType.Pawn, true).Count + 1) * board.GetPieceList(PieceType.Rook, false).Count +
+                scores[4] * board.GetPieceList(PieceType.Queen, false).Count;
+
+
+        return eval;
     }
 }
 
 public class SearchResult {
-    public Move best {get; }
-    public int evaluation {get; }
+    public Move best {get; set;}
+    public double evaluation {get; }
 
-    public SearchResult(Move best, int evaluation){
+    public SearchResult(Move best, double evaluation){
         this.best = best;
         this.evaluation = evaluation;
     }
