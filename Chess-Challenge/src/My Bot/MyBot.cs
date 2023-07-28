bab8fb2291ff9b24df32ef1ce25076623e047e9a
joshua_beard <matinzzz@icloud.com>
2023-07-27 23:24:21 -0700

    advanced search started

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,5 +1,6 @@
 ﻿using ChessChallenge.API;
 using System.Collections.Generic;
+using System;
 
 public class MyBot : IChessBot
 {
@@ -12,24 +13,86 @@ public class MyBot : IChessBot
     {
         Move best = Move.NullMove;
         for (int i = 1; timer.MillisecondsElapsedThisTurn < timePerMove; i++){
-            best = Search(board, timer, i, double.MinValue, double.MaxValue, board.IsWhiteToMove).best;
+            best = Search(board, timer, board.GameMoveHistory[board.GameMoveHistory.GetLength(0) - 1], i, int.MinValue, int.MaxValue, board.IsWhiteToMove).best;
         }
         return best;
     }
 
 
     
-    public SearchResult Search(Board board, Timer timer, int depth, double a, double b, bool maximizer){
+    public SearchResult Search(Board board, Timer timer, Move lastMove, int depth, int a, int b, bool maximizer){
+        if (depth == 0 || board.IsInCheckmate() || board.IsDraw())
+        {
+            return new SearchResult(lastMove, StaticEvaluation(board));
+        }
 
+        if (maximizer)
+        {
+            SearchResult max = new SearchResult(Move.NullMove, int.MinValue);
+
+            foreach (Move move in board.GetLegalMoves())
+            {
+                board.MakeMove(move);
+                SearchResult result = Search(board, timer, move, depth - 1, a, b, false);
+                board.UndoMove(move);
+
+                max = result.Max(max);
+
+                a = Math.Max(a, result.evaluation);
+                if (b <= a)
+                {
+                    break;
+                }
+            }
+            return max;
+        }
+        else
+        {
+            SearchResult min = new SearchResult(Move.NullMove, int.MaxValue);
+
+            foreach (Move move in board.GetLegalMoves())
+            {
+                board.MakeMove(move);
+                SearchResult result = Search(board, timer, move, depth - 1, a, b, true);
+                board.UndoMove(move);
+
+                min = result.Min(min);
+
+                b = Math.Min(b, result.evaluation);
+                if (b <= a)
+                {
+                    break;
+                }
+            }
+            return min;
+        }
+    }
+
+    public int StaticEvaluation(Board b){
+        return 0;
     }
 }
 
-class SearchResult {
+public class SearchResult {
     public Move best {get; }
     public int evaluation {get; }
 
     public SearchResult(Move best, int evaluation){
         this.best = best;
         this.evaluation = evaluation;
+    }
+
+    public SearchResult Max(SearchResult other){
+        if (this.evaluation > other.evaluation){
+            return this;
+        }
+        return other;
+    }
+
+    public SearchResult Min(SearchResult other){
+        if (this.evaluation < other.evaluation){
+            return this;
+        }
+        return other;
     }
 }
