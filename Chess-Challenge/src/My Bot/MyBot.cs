d4d52496925b4166acacdd9c6248d254d688943c
joshua_beard <matinzzz@icloud.com>
2023-07-27 22:40:10 -0700

    backup

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,18 +1,25 @@
 ﻿using ChessChallenge.API;
+using System.Collections.Generic;
 
 public class MyBot : IChessBot
 {
 
     // prep for 50 move game
     const int timePerMove = 1200; //MS
+    static IDictionary <Board, double> transposition = new Dictionary<Board, double>();
 
     public Move Think(Board board, Timer timer)
     {
-       return Move.NullMove;
+        Move best = Move.NullMove;
+        for (int i = 1; timer.MillisecondsElapsedThisTurn < timePerMove; i++){
+            best = Search(board, timer, i, )
+        }
+        return best;
     }
 
+
     
-    public SearchResult Search(Board board, Timer timer, int depth, double a, double b){
+    public SearchResult Search(Board board, Timer timer, int depth, double a, double b, bool maximizer){
         
     }
 }
