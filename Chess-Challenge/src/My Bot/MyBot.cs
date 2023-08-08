1c3c1ecc89a2445e9e4d6deb26269d1e8c735393
Yoursole <80355246+Yoursole1@users.noreply.github.com>
2023-08-07 18:21:27 -0700

    .

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -8,7 +8,17 @@ public class MyBot : IChessBot
     // prep for 50 move game
     const int timePerMove = 900; //MS 1200
 
-    static double[] scores = {1, 3, 3.5, 5, 9};
+    private readonly ulong[,] PST = {
+        {2222222228888988866, 9678666444674443345, 1633333345333333003, 1332222222200000000},
+        {5022222202333333223, 4999932239999322399, 1993223799932233333, 1320522225000000000},
+        {3033333300333333013, 6344331155995514458, 1854444455444144554, 1410111111000000000},
+        {1000000006669966600, 2000000000000000000, 9000000000000000000, 1000004440000000000},
+        {5013333101344443134, 6777743347997433479, 1974334444443134444, 1310133331000000000},
+        {3333333333333333333, 2333333333993333339, 1933333333333333003, 1333390039300000000}
+    };
+    // To be used when the enemy king is all alone (or with only pawns left maybe, to push the king to the sides of the board)
+    // private ulong[] KING_EDGE_PST = {};
+    private int[] PIECES = {100, 300, 350, 500, 900, 0};
 
     public Move Think(Board board, Timer timer)
     {
@@ -43,7 +53,7 @@ public class MyBot : IChessBot
         
         if (depth <= 0 || board.IsInCheckmate() || board.IsDraw())
         {
-            return new SearchResult(lastMove, color * StaticEvaluation(board));
+            return new SearchResult(lastMove, color * Static_Evaluation(board));
         }
 
         if (timer.MillisecondsElapsedThisTurn >= timePerMove)
@@ -95,9 +105,8 @@ public class MyBot : IChessBot
     }
 
 
-    private double StaticEvaluation(Board board)
-    {
-        double eval = 0;
+    public int Static_Evaluation(Board board){
+        int eval = 0;
 
         if (board.IsDraw())
         {
@@ -108,72 +117,63 @@ public class MyBot : IChessBot
         if (board.IsWhiteToMove)
         {
             if(board.IsInCheckmate()){
-                return double.MinValue;
+                return int.MinValue + 1;
             }
             if(board.IsInCheck()){
-                eval -= 0.5;
+                eval -= 300;
             }
         }
 
         if (!board.IsWhiteToMove)
         {
             if(board.IsInCheckmate()){
-                return double.MaxValue;
+                return int.MaxValue - 1;
             }
             if(board.IsInCheck()){
-                eval += 0.5;
+                eval += 300;
             }
         }
 
+        foreach (PieceType type in Enum.GetValues(typeof(PieceType))){
 
-
-        eval += scores[0] * board.GetPieceList(PieceType.Pawn, true).Count +
-                (scores[1] + (1/8) * board.GetPieceList(PieceType.Pawn, true).Count - 1) * board.GetPieceList(PieceType.Knight, true).Count +
-                scores[2] * board.GetPieceList(PieceType.Bishop, true).Count +
-                (scores[3] + (-1/8) * board.GetPieceList(PieceType.Pawn, true).Count + 1) * board.GetPieceList(PieceType.Rook, true).Count +
-                scores[4] * board.GetPieceList(PieceType.Queen, true).Count;
-
-
-        eval -= scores[0] * board.GetPieceList(PieceType.Pawn, false).Count +
-                (scores[1] + (1/8) * board.GetPieceList(PieceType.Pawn, true).Count - 1) * board.GetPieceList(PieceType.Knight, false).Count +
-                scores[2] * board.GetPieceList(PieceType.Bishop, false).Count +
-                (scores[3] + (-1/8) * board.GetPieceList(PieceType.Pawn, true).Count + 1) * board.GetPieceList(PieceType.Rook, false).Count +
-                scores[4] * board.GetPieceList(PieceType.Queen, false).Count;
-
-        eval += developmentEval(board);
-
-        return eval;
-    }
-
-
-    public double developmentEval(Board b){
-        double eval = 0;
-
-        if (b.GameMoveHistory.Length < 10){
-            foreach (Piece p in b.GetPieceList(PieceType.Knight, true)){
-            if(p.Square.Rank == 0){
-                eval -= 1;
+            if (type == PieceType.None){ 
+                continue;
             }
-            }
-            foreach (Piece p in b.GetPieceList(PieceType.Knight, false)){
-                if(p.Square.Rank == 7){
-                    eval += 1;
+            ulong bitboardW = board.GetPieceBitboard(type, true);
+            ulong b = board.GetPieceBitboard(type, false);
+            ulong bitboardB = 0;
+
+            for (int i = 0; i < 64; i++){
+                if ((b & (1UL << i)) != 0){
+                    int reversedIndex = 63 - i;
+                    bitboardB |= 1UL << reversedIndex;
                 }
             }
 
-            foreach (Piece p in b.GetPieceList(PieceType.Bishop, true)){
-                if(p.Square.Rank == 0){
-                    eval -= 0.5;
-                }
+            eval += PIECES[(int)type - 1] * (board.GetPieceList(type, true).Count - board.GetPieceList(type, false).Count);
+
+
+
+            for (int i = 63; i >= 0; i--){ // first bit read is bottom left
+                int bitW = (int)((bitboardW >> i) & 1);
+                int bitB = (int)((bitboardB >> i) & 1);
+
+                int id = (int) Math.Ceiling((i + 0.9D) % 8 - 8 * Math.Floor((double) (i + 0.9) / 8) + 55);
+                int subArray = (int) Math.Floor(4 * id / 72D);
+                id -= subArray * 18 - 1;
+
+                ulong pst = PST[(int)type - 1, subArray];
+                int multiplier = (int) (pst / Math.Pow(10, 18 - id) % 10);
+
+                multiplier -= PST[(int)type - 1, 2] / Math.Pow(10, 18) == 9 ? 0 : (int)(PST[(int)type - 1, 0] / (ulong) Math.Pow(10, 18));
+                
+                multiplier *= (int)(PST[(int)type - 1, 1] / (ulong) Math.Pow(10, 18));
+                eval += bitW * multiplier;
+                eval -= bitB * multiplier;
             }
-            foreach (Piece p in b.GetPieceList(PieceType.Bishop, false)){
-                if(p.Square.Rank == 7){
-                    eval += 0.5;
-                }
-            }
+
         }
         
-
         return eval;
     }
 }
